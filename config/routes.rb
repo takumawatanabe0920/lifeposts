e020b65ff4a369bdf50178680013e3ae3c7f5db4
takuma watanabe <takuma_0920@docomo.ne.jp>
2019-03-01 09:25:20 +0000

    aaa

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
+  get "signup", to:"users#new"
+  get "login", to:"sessions#new"
+  post "login", to:"sessions#create"
+  delete "logout", to:"sessions#destroy"
+  resources :users
 end
